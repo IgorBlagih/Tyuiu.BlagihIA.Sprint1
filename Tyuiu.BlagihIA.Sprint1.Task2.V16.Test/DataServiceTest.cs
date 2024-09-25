e862d75566190f4739d563ada901f6827058a834
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-09-25 18:52:47 +0500

    Task2

---
+++ Tyuiu.BlagihIA.Sprint1.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.BlagihIA.Sprint1.Task2.V16.Lib;
+namespace Tyuiu.BlagihIA.Sprint1.Task2.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExprassion()
+        {
+            DataService ds= new DataService();
+            int r = 2;
+            var res = ds.CalculatePerimetrCircle(r);
+            Assert.AreEqual(12.56, res);
+        }
+    }
+}
---
+++ Tyuiu.BlagihIA.Sprint1.Task2.V16/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BlagihIA.Sprint1.Task2.V16.Lib;
+namespace Tyuiu.BlagihIA.Sprint1.Task2.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Благих И.А | ИИПб-24-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки  работы в С#                                       *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Благих Игорь Александрович | ИИПб-24-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу которая вычисляет периметр круга                     *");
+            Console.WriteLine("* и выводит результат на экран                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            int r;
+
+            Console.WriteLine("Введите радиус:");
+            r = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Периметр круга = " + ds.CalculatePerimetrCircle(r));
+            Console.ReadKey();
+        }
+    }
+}
